190c614bf59358b1a511d627e0d298b1ed38a892
Jared Keyes <jared.keyes3@gmail.com>
2026-05-24 02:25:16 -0400

    Working on build gold more

--- src/transformations/build_gold.py
+++ src/transformations/build_gold.py
@@ -18,3 +18,16 @@ def main():
     spark = get_spark()
 
     GOLD_DIR.mkdir(parents=True, exist_ok=True)
+
+    cloudtrail = spark.read.parquet(str(SILVER_DIR / "cloudtrail_enriched"))
+    guardduty = spark.read.parquet(str(SILVER_DIR / "guardduty_clean"))
+    iam_users = spark.read.parquet(str(SILVER_DIR / "iam_users_clean"))
+
+    security_findings_enriched = (
+        guardduty
+        .join(
+            cloudtrail,
+            guardduty.related_event_id == cloudtrail.event_id,
+            "left"
+        )
+    )
